835542dc6c719d1a359ea26ed532f7db486b163e
Jacques Camier <jacquescamier@bartolottas.com>
2016-08-30 18:00:04 -0500

    Modified json converter

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -5,25 +5,35 @@ import json
 
 with open('gl_table.csv') as csvfile:
     csvfile = csv.DictReader(csvfile)
+    model_name = 'gl_table'
+    field_1 = 'gl_acct'
+    field_2 = 'gl_desc'
+    x = 0
     output = []
     for each in csvfile:
+        x += 1
         row = {}
-        row = {"model": "gl_table"}
-        row['gl_acct'] = each['gl_acct']
-        row['gl_desc'] = each['gl_desc']
+        row = {'model': model_name, 'pk': x, 'fields': ({field_1: each['gl_acct'], field_2: each['gl_desc']})}
         output.append(row)
-    json.dump(output,open('converted_file.json','w'), indent=4, sort_keys=False)
+    json.dump(output, open('converted_file.json','w'), indent=4, sort_keys=False)
 
 
-
-
-
-
-
-# output =[]
-# for each in csvfile:
-#     row = {}
-#     row['gl_acct'] = each['gl_acct']
-#     row['gl_desc'] = each['gl_desc']
-#     output.append(row)
-# json.dump(output,open('filename.json','w'), indent=4, sort_keys=False)
+#........ json data model example......
+# [
+#   {
+#     "model": "myapp.person",
+#     "pk": 1,
+#     "fields": {
+#       "first_name": "John",
+#       "last_name": "Lennon"
+#     }
+#   },
+#   {
+#     "model": "myapp.person",
+#     "pk": 2,
+#     "fields": {
+#       "first_name": "Paul",
+#       "last_name": "McCartney"
+#     }
+#   }
+# ]
